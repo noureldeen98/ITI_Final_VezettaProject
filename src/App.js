458ec6689c5507bb70ebf09e5b1f764261f32bda
Yussef Mohamed <yussefmohamed@Naders-MacBook.local>
2022-02-01 16:05:54 +0200

    Adding pharmacy section

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
+import React from "react";
+import "./App.css";
+import Pharmacy from "./Components/Pharmacy/Pharmacy";
 
-import React from 'react';
-import './App.css';
-import Doctorcard from './Components/Card/DoctorCard'
-import Footer from './Components/home/footer/Footer';
-import NavbarComponent from './Components/home/header/NavbarComponent';
-import HomeSlider from './Components/home/homeSlider/HomeSlider';
-import SmallPharmacy from './Components/home/part2/sPharmacy/SmallPharmacy';
-import Corona from './Components/home/part3/corona/Corona';
-import DeptSlider from './Components/home/part4/DeptSlider';
-import Part5 from './Components/home/part5/Part5';
-import Part6 from './Components/home/part6/Part6';
 
 function App() {
   return (
     <React.Fragment>
-        <NavbarComponent />
-        <HomeSlider />
-        <SmallPharmacy />
-        <Corona />
-        <DeptSlider />
-        <Part5 />
-        <Part6 />
-        <Footer />
-      {/* <TabToggle /> */}
-    {/* <h1>Vezeeta website</h1>
-    <button className='btn btn-danger'>HI</button>
-    <Doctorcard/> */}
+      <Pharmacy />
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
--- src/ReactRedux/combineReducer.js
+++ src/ReactRedux/combineReducer.js
@@ -1,20 +1,14 @@
 import { combineReducers } from "redux";
 import { allAreasReducer } from "./Reducers/AreasReducerRedux";
 import { allCitiesReducer } from "./Reducers/CitiesReducerRedux";
-import deptReducer from './Reducers/DepartmentsReducer'
-
+import deptReducer from "./Reducers/DepartmentsReducer";
 import { allDeptsReducer } from "./Reducers/DeptsReducerRedux";
-const combineReducer = combineReducers({
-    deptRed: deptReducer,
-    dptReduxReducer: allDeptsReducer,
-    citiesReduxReducer: allCitiesReducer,
-    areasReduxReducer: allAreasReducer
 
-import doctorCollectionReducer from '../ReactRedux/Reducers/ReducerOfFetchingCollectionsofDoctors'
 const combineReducer = combineReducers({
-    deptRed: deptReducer,
-    doctorCollectionReducer :doctorCollectionReducer,
-
-})
+  deptRed: deptReducer,
+  dptReduxReducer: allDeptsReducer,
+  citiesReduxReducer: allCitiesReducer,
+  areasReduxReducer: allAreasReducer,
+});
 
 export default combineReducer;

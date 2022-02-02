89a9c8d83f53b761e14ed9f7c8bb8bff290d0554
yusseftawfik <yussefmohamed96@gmail.com>
2022-02-02 14:15:02 +0200

    linking pharmacy section with the website

--- src/Components/Pharmacy/Map/Map.jsx
+++ src/Components/Pharmacy/Map/Map.jsx
@@ -3,5 +3,10 @@ import React from 'react';
 export default function Map() {
     return (
         <>
+            <iframe title="pharmacies map"
+                width="100%"
+                src="https://www.google.com/maps/d/u/0/embed?mid=1bWspxicUNuCruZlnlErXVUfTIvQ9YKPl&ehbc=2E312F"
+                height="500">
+            </iframe>
         </>);
 }
--- src/Components/home/part2/sPharmacy/SmallPharmacy.jsx
+++ src/Components/home/part2/sPharmacy/SmallPharmacy.jsx
@@ -3,12 +3,20 @@ import style from './smallPharmacy.module.css';
 import pic1 from '../../../../Images/part2/part2Img1.png';
 import pic2 from '../../../../Images/part2/part2Img2.png';
 import CallVisitLink from '../call-visit-link/CallVisitLink';
+import { Link } from 'react-router-dom';
+import Pharmacy from '../../../Pharmacy/Pharmacy';
+
+
 export default function SmallPharmacy() {
     let data = [
-        {title: 'مكالمة دكتور', id: 1,
-        body: 'للمتابعه عبر مكالمة صوتية او فيديو', img: pic1},
-        {title: 'زيارة منزلية', id: 2,
-        body: 'اختار التخصص والدكتور هيجيلك البيت', img: pic2}
+        {
+            title: 'مكالمة دكتور', id: 1,
+            body: 'للمتابعه عبر مكالمة صوتية او فيديو', img: pic1
+        },
+        {
+            title: 'زيارة منزلية', id: 2,
+            body: 'اختار التخصص والدكتور هيجيلك البيت', img: pic2
+        }
     ]
     return <>
         <div className={`${style.p} row g-0`}>
@@ -17,15 +25,15 @@ export default function SmallPharmacy() {
                     <h1 className={`py-4 ${style.part2Head}`}>حلول جديدة لرعاية أفضل ليك</h1>
                     <div className={`card ${style.part2Card} flex-sm-row`}>
                         <div className="d-md-none">
-                            
+
                         </div>
                         <div className="card-body">
-                            <h2 className={`card-title ${style.cT}`}>صيدلية</h2> 
+                            <h2 className={`card-title ${style.cT}`}>صيدلية</h2>
                             <p className="card-text">
                                 اطلب ادويتك وكل اللى تحتاجه من الصيدلية
                             </p>
-                            <a href="./pharmacy.html" 
-                            className={`btn btn-light px-5`}>اطلب الان</a>
+                            <Link to='/pharmacy'
+                                className={`btn btn-light px-5`}>اطلب الان</Link>
                         </div>
                     </div>
                 </div>
@@ -33,7 +41,7 @@ export default function SmallPharmacy() {
 
             <div className={`row ${style.part2b} justify-content-center mx-0 my-4`}>
                 {/* hena ba2i l components */}
-                {data.map(d=> (
+                {data.map(d => (
                     <CallVisitLink data={d} key={d.id} />
                 ))}
             </div>

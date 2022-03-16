c9016e18b7fec933f7dcc766a288f49b3430e549
SarahYouis <sarahyounis604@gmail.com>
2022-03-16 15:33:16 +0200

    AuthUser

--- src/Components/Contact/Signin/signin.jsx
+++ src/Components/Contact/Signin/signin.jsx
@@ -1,72 +1,115 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { auth } from "../../../FireBaseConfiguration/FirebaseConfiguration";
 import NavbarComponent from "../../home/header/NavbarComponent";
 import Footer from "../../home/footer/Footer";
-import style from "../style/style.css";
-import { Link } from 'react-router-dom';
+import "../style/style.css";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+
 
 // الدخول
 export default function Signin() {
 
+    const [Emaile, setUserEmaile] = useState("");
+    const [pass, setPass] = useState("");
+    const [user, setUser] = useState({});
+    const history = useHistory();
+
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
+
+    const logInWithEmailAndPassword = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, Emaile, pass);
+            localStorage.setItem("UserEmail", user.user.reloadUserInfo.email);
+            history.push('/home');
+
+            // console.log(user);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const logout = async () => {
+        await signOut(auth);
+        localStorage.removeItem("User")
+    };
+
     return (
         <>
             <NavbarComponent />
-            <div className="row w-75" id="form">
-                <form className="needs-validation" action="./اتصل بنا1.html" onsubmit="LoginForm(event)" novalidate>
+            <div className="row w-75 py-4" id="form">
+                <form className="needs-validation" noValidate>
                     <div id="one" className="row">
-                        <p style={{ marginBottom: " 0%" }}>انضم الان</p>
+                        <p style={{ marginBottom: " 0%" }} className="text-center">انضم الان</p>
                     </div>
-                    <div className="row">
+                    {/* <div className="row">
                         <button type="submit" id="butt">
                             فعل حسابك من الفيسبوك
                         </button>
-                    </div>
+                    </div> */}
                     <hr />
                     {/* one */}
-                    <div className="row iteam-form">
+                    <div className="row iteam-form ">
                         <div className="col-lg-3  col-sm-12">
-                            <label for="validationCustom01">الموبايل او البريد الالكتروني</label>
+                            <label htmlFor="validationCustom01">الموبايل او البريد الالكتروني</label>
                             <sup style={{ color: "red" }}> *</sup>
                         </div>
                         <div className="col-lg-9">
                             <input type="text" className="form-control container-fluid" id="Email" required
-                                placeholder="الموبايل" />
+                                placeholder="الموبايل"
+                                onChange={(event) => { setUserEmaile(event.target.value); }}
+                                value={Emaile} />
                             <div className="valid-feedback">الموبايل او البريد الالكتروني</div>
                         </div>
                     </div>
                     {/* two */}
                     <div className="row iteam-form">
                         <div className="col-lg-3  col-sm-12">
-                            <label for="validationCustom02">كلمة المرور</label>
+                            <label htmlFor="validationCustom02">كلمة المرور</label>
                             <sup style={{ color: "red" }}> *</sup>
                         </div>
 
                         <div className="col-lg-9">
-                            <input type="password" className="form-control container-fluid" placeholder="كلمة المرور" id="password" required />
+                            <input type="password" className="form-control container-fluid" placeholder="كلمة المرور" id="password" required
+                                onChange={(event) => { setPass(event.target.value); }}
+                                value={pass} />
                             <div className="valid-feedback">كلمة المرور</div>
                         </div>
                     </div>
                     <hr className="mx-2" />
 
                     <div className="iteam-form">
-                        <div className="row p-2" style={{textAlign:"center"}}>
+                        <div className="row" style={{ textAlign: "center" }}>
                             <div className="form-check">
-
-                                <label className="form-check-label ms-5" for="gridCheck1">
-                                    <input className="form-check-input" type="checkbox" id="gridCheck1"/>
-                                        تذكرني
+                                {/* <label className="form-check-label ms-5" htmlFor="gridCheck1">
+                                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                                    تذكرني
                                 </label>
-                                <label className="form-check-label me-5" for="gridCheck1">
+                                <label className="form-check-label me-5" htmlFor="gridCheck1">
                                     <a href="#"> نسيت كلمةالمرور</a>
-                                </label>
+                                </label> */}
                             </div>
                         </div>
 
-                        <div style={{width:"30%", margin:"auto"}} className="row my-2">
-                            <button type="submit" className="btn btn-danger">دخول</button>
+                        <div className="row">
+                            <button type="button" className="btn btn-danger"
+                                onClick={() => logInWithEmailAndPassword(Emaile, pass)} style={{ width: "150px" }}>دخول</button>
                         </div>
-                        <div className="row"style={{textAlign:"center"}}>
-                            <p> لست مسجل في فيزيتا؟ <Link to="/Signup" style={{textDecoration:"underline"}}>انضم الان</Link></p>
+
+                        <div className="row py-4" style={{ textAlign: "center" }}>
+                            {user?.email}
+                            <p> لست مسجل في فيزيتا؟ <Link to="/Signup" style={{ textDecoration: "underline" }}>انضم الان</Link></p>
                         </div>
+
+
+                        <div>
+                            <button onClick={logout}> Sign Out </button>
+                        </div>
+
                     </div>
                 </form>
             </div>
--- src/Components/Contact/Signup/signup.jsx
+++ src/Components/Contact/Signup/signup.jsx
@@ -1,79 +1,111 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavbarComponent from "../../home/header/NavbarComponent";
 import Footer from "../../home/footer/Footer";
-import style from "../style/style.css"
-import {Link} from 'react-router-dom'
-// انضم الان 
-export default function Signup() {
+import "../style/style.css"
+import { Link } from 'react-router-dom'
+import { db, auth } from '../../../FireBaseConfiguration/FirebaseConfiguration'
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { collection, getDocs, addDoc } from 'firebase/firestore'
+import { useHistory } from 'react-router-dom';
 
+// انضم الان
+export default function Auth() {
+    const [Name, setUserName] = useState("");
+    const [Emaile, setUserEmaile] = useState("");
+    const [pass, setPass] = useState("");
+    const [Phone, setUserPhone] = useState("");
+    const [DateBirth, setUserDateBirth] = useState("");
+    const history = useHistory();
 
+    // signup
+    const registerWithEmailAndPassword = async (email, password) => {
+
+        try {
+            const auth2 = getAuth();
+            const res = await createUserWithEmailAndPassword(auth2, email, password);
+            const user = res.user;
+            await addDoc(collection(db, "Users"), {
+                uid: user.uid,
+                Name: Name,
+                Emaile: user.email,
+                Phone: Phone,
+                DateBirth: DateBirth
+            });
+
+            history.push('/home')
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     return (
 
         <div>
             <NavbarComponent />
             <div className='row w-75' id="form">
-                <form className="needs-validation" action="./Home.html" onsubmit="Done(event)" novalidate>
+                <form className="needs-validation" noValidate>
                     <div id="one" className="row">
-                        <p style={{ marginBottom: " 0%" }}>انضم الان</p>
+                        <p style={{ marginBottom: " 0%" }} className="text-center">انضم الان</p>
                     </div>
-                    <div className="row">
+                    {/* <div className="row">
                         <button type="submit" id="butt">
                             فعل حسابك من الفيسبوك
                         </button>
-                    </div>
+                    </div> */}
                     <hr />
                     {/* one */}
                     <div className="row iteam-form">
                         <div className="col-lg-3  col-sm-12">
-                            <label for="validationCustom01">الاسم</label>
+                            <label htmlFor="validationCustom01">الاسم</label>
                             <sup style={{ color: "red" }}> *</sup>
                         </div>
                         <div className="col-9">
-                            <input type="text" className="form-control container-fluid" id="FullName" required
-                                placeholder="الاسم بالكامل" itemid="" />
+                            <input type="text" className="form-control container-fluid" id="FullName"
+                                required onChange={(event) => { setUserName(event.target.value) }} placeholder="الاسم بالكامل" />
                             <div className="valid-feedback">يرجى ادخال الاسم</div>
                         </div>
                     </div>
                     {/* two */}
                     <div className="row iteam-form">
                         <div className="col-lg-3  col-sm-12">
-                            <label for="validationCustom02">رقم الموبايل</label>
+                            <label htmlFor="validationCustom02">رقم الموبايل</label>
                             <sup style={{ color: "red" }}> *</sup>
                         </div>
 
                         <div className="col-lg-9">
-                            <input type="text" class="form-control container-fluid" required placeholder="رقم الموبايل" id="Phone" />
+                            <input type="text" className="form-control container-fluid" required onChange={(event) => { setUserPhone(event.target.value); }} placeholder="رقم الموبايل" id="Phone" />
                             <div className="valid-feedback">يرجى ادخال رقم الموبايل</div>
                         </div>
                     </div>
                     {/* three */}
                     <div className="row iteam-form">
                         <div className="col-lg-3  col-sm-12">
-                            <label for="validationCustomUsername">البريدالالكتروني</label>
+                            <label htmlFor="validationCustomUsername">البريدالالكتروني</label>
                             <sup style={{ color: "red" }}> *</sup>
                         </div>
-                        <div class="col-lg-9">
-                            <input type="text" class="form-control container-fluid" aria-describedby="inputGroupPrepend"
-                                required placeholder="البريد الالكتروني" id="Email" />
+                        <div className="col-lg-9">
+                            <input type="text" className="form-control container-fluid" aria-describedby="inputGroupPrepend"
+                                required onChange={(event) => { setUserEmaile(event.target.value); }}
+                                value={Emaile}
+                                placeholder="البريد الالكتروني" id="Email" />
                             <div className="invalid-feedback">
                                 يرجى ادخال البريدالالكتروني
                             </div>
                         </div>
                         {/* four */}
-                        <div className="row iteam-form">
-                            <div className="col-lg-3  col-sm-12">
-                                <label for="validationCustomUsername">النوع</label>
+                        <div className="row iteam-form py-3">
+                            <div className="col-lg-3  col-sm-12 pt-2">
+                                <label htmlFor="validationCustomUsername">النوع</label>
                                 <sup style={{ color: "red" }}> *</sup>
                             </div>
-                            <div className="col-lg-9">
+                            <div className="col-lg-9 pt-3">
                                 <label style={{ paddingLeft: "2rem" }}>
                                     انثي
-                                    <input type="radio" name="type" id="male" autocomplete="off" />
+                                    <input type="radio" name="type" id="male" autoComplete="off" />
                                 </label>
-                                <label style={{ paddingLeft: "2rem;" }}>
+                                <label style={{ paddingLeft: "2rem" }}>
                                     ذكر
-                                    <input type="radio" name="type" id="female" autocomplete="off" />
+                                    <input type="radio" name="type" id="female" autoComplete="off" />
                                 </label>
                             </div>
 
@@ -81,22 +113,27 @@ export default function Signup() {
                         {/* five */}
                         <div className="row iteam-form">
                             <div className="col-lg-3  col-sm-12">
-                                <label for="validationCustom01"> تاريخ الميلاد</label>
+                                <label htmlFor="validationCustom01"> تاريخ الميلاد</label>
                             </div>
 
                             <div className="col-lg-9">
-                                <input type="text" className="form-control container-fluid" placeholder="تاريخ الميلاد" id="DateBirth" />
+                                <input type="text" className="form-control container-fluid" placeholder="تاريخ الميلاد" id="DateBirth"
+                                    onChange={(event) => { setUserDateBirth(event.target.value); }} />
                             </div>
                         </div>
                         {/* six */}
                         <div className="row iteam-form">
                             <div className="col-lg-3  col-sm-12">
-                                <label for="validationCustom01">كلمه المرور</label>
+                                <label htmlFor="validationCustom01">كلمه المرور</label>
                                 <sup style={{ color: "red" }}> *</sup>
                             </div>
 
                             <div className="col-lg-9">
-                                <input type="password" className="form-control container-fluid" required placeholder="كلمه المرور" id="Password" />
+                                <input type="password" className="form-control container-fluid"
+                                    required placeholder="كلمه المرور" id="Password"
+                                    onChange={(event) => { setPass(event.target.value); }}
+                                    value={pass}
+                                />
                                 <div className="valid-feedback">يرجى ادخال كلمه المرور </div>
                             </div>
                         </div>
@@ -104,20 +141,22 @@ export default function Signup() {
                         <div className="row iteam-form">
                             <div className="col-lg-3 col-md-12"></div>
                             <div className="form-check form-switch col-lg-9 col-md-12">
-                                <label clclassNameass="form-check-label" for="flexSwitchCheckDefault">
+                                {/* <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
                                     <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
                                     إضافة التأمين الطبي
-                                </label>
+                                </label> */}
                                 <p>بقيامك بالتسجيل فانت توافق على الشروط و القوانين</p>
-                                <a href="./دخول.html"><button type="submit" className="btn btn-danger" id="sign-up" onclick="Save()">اشترك الان</button></a>
+
+                                <button type='button' className="btn btn-danger" id="sign-up"
+                                    onClick={() => registerWithEmailAndPassword(Emaile, pass)}>اشترك الان</button>
                             </div>
 
                         </div>
 
-                        <div style={{clear:"both", margin:"0%"}}></div>
+                        <div style={{ clear: "both", margin: "0%" }}></div>
                         <hr className="mx-2" />
-                        <div className="row" style={{textAlign:"center"}}>
-                            <p>مسجل بالفعل في فيزيتا؟ <Link to="/Signin" style={{textDecoration:"underline"}}>دخول</Link></p>
+                        <div className="row" style={{ textAlign: "center" }}>
+                            <p>مسجل بالفعل في فيزيتا؟ <Link to="/Signin" style={{ textDecoration: "underline" }}>دخول</Link></p>
                         </div>
                     </div>
                 </form>
